f436ae3200e7db5654728497243d27236b0b87f8
simon <simonmartinez1605@gmail.comgit config --global user.email simonmartinez1605@gmail.com>
2024-03-16 13:00:00 -0500

    routers

---
+++ app.js
@@ -0,0 +1,20 @@
+//modulos requeridos
+const express = require("express");
+const cors = require("cors");
+const routerApi = require("./routes"); 
+require("dotenv").config();
+
+
+//asignacion de express a la app
+const app = express();
+
+app.use(cors());
+
+
+app.use(express.json());
+
+routerApi(app); 
+//Escuchar el puerto en el que se van a ejecutar los datos
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Escuchando el puerto ${port}`)); 
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express'); 
+
+const signatureBerry = require('./sigantureBerry'); 
+const facturacionBerry =require('./facturacion'); 
+
+function routerApi (app){
+    const router = express.Router(); 
+
+    app.use('/api/v1', router); 
+
+    router.use('/api/Signature', signatureBerry); 
+    router.use('/api/res/nidum', facturacionBerry); 
+}
+
+module.exports = routerApi; 
